eb983791a200f949a1da5cf434ebcba2a7d035f2
manho <86037446+manho30@users.noreply.github.com>
2021-12-14 11:00:31 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <base target="_top">
+        <meta charset="UTF-8">
+        <script src="handleAction.js"></script>
+        <script src="callbackQuery.js"></script>
+        <script src="newMember"></script>
+        <script src="postTelegram.js"></script>
+    </head>
+  <body>
+      <script>
+          var payload = preparePayload;
+          console.log(payload);
+          console.log(JSON.stringify(payload));
+      </script>
+      <h1>
+          Hello world from Man Ho 
+      </h1>
+  </body>
+</html>
